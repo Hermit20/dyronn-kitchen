24e5f89b2010803f113c25b5195b6d0189fa5ef7
Hermit20 <olojede30@gmail.com>
2025-03-01 15:49:07 -0500

    more changes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
-
+import { BrowserRouter } from 'react-router-dom';
 const container = document.getElementById('root');
 
 // Create the root using the new 'createRoot' API.
 const root = ReactDOM.createRoot(container);
 
 // Render your main App component
-root.render(<App />);
+root.render(
+<React.StrictMode>
+    <BrowserRouter basename='/dyronn-kitchen'>
+    <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
