3b88aef9416f5ef73b1e2f5675de01c5621fc4cc
Hermit20 <olojede30@gmail.com>
2025-03-01 15:46:11 -0500

    changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
         </a>
       </div>
       <ul className="app__navbar-links">
-        <li className="p__opensans"><a href="/dyronn-kitchen/">Home</a></li>
+        <li className="p__opensans"><a href="dyronn-kitchen/">Home</a></li>
         <li className="p__opensans"><a href="/#about">About</a></li>
         <li className="p__opensans"><a href="/menu">Menu</a></li>
         {/* <li className="p__opensans"><a href="#awards">Awards</a></li> */}
